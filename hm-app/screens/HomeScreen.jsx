e9538952d691b41bf1032c61d0b38342647639ea
ifanramadhany <ifanpersie14@gmail.com>
2022-01-04 13:47:50 +0700

    jan4/13:47

--- hm-app/screens/HomeScreen.jsx
+++ hm-app/screens/HomeScreen.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import {
   StyleSheet,
   View,
@@ -6,23 +6,118 @@ import {
   ImageBackground,
   Image,
   TouchableOpacity,
-  StatusBar
+  StatusBar,
 } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { FontAwesome, Ionicons } from "react-native-vector-icons";
+import { Badge } from "react-native-elements";
 
 export default function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={{fontWeight: 'bold', color: 'red'}}>Ini adalah Home Screen</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.wrapperAllContnet}>
+        <View style={styles.helloUser}>
+          <View style={styles.helloUserLeft}>
+            <View style={styles.topLeft}>
+              <Text style={{ color: "#4b5563", fontSize: 14 }}>
+                DECEMBER 05, 2022
+              </Text>
+            </View>
+            <View style={styles.bottomLeft}>
+              <View style={styles.bottomLeftIn}>
+                <Text style={{ fontSize: 19, marginRight: 8 }}>Hello,</Text>
+                <Text style={{ fontSize: 19, fontWeight: "bold" }}>
+                  Mikael Johnson
+                </Text>
+              </View>
+            </View>
+          </View>
+          <View style={styles.helloUserRight}>
+            <View style={styles.notification}>
+              <Ionicons style={styles.notifStyle} name="notifications" size={40} color="#1f2937"/>
+              <Badge
+                value="7"
+                status="error"
+                badgeStyle={{scaleX: 1.1, scaleY: 1.1}}
+                containerStyle={styles.badgeStyle}
+              />
+              {/* <View style={styles.badgeStyleManual}>
+                <Text style={{color: '#fff', fontSize: 11}}>70+</Text>
+              </View> */}
+            </View>
+          </View>
+        </View>
+      </View>
       <StatusBar style="auto" />
-    </View>
-  )
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+  notifStyle: {
+    marginTop: 5,
+    marginRight: 5
+  },
+  badgeStyle: {
+    position: "absolute",
+    top: 5,
+    right: 4,
+  },
+  badgeStyleManual: {
+    position: "absolute",
+    top: -1,
+    right: -1,
+    backgroundColor: 'red',
+    borderRadius: 150,
+    padding: 3,
+    borderWidth: 1,
+    borderColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  notification: {
+    // backgroundColor: "yellow",
+    width: "60%",
+    height: "60%",
+    alignItems: 'flex-end',
+    justifyContent: 'flex-start',
+  },
+  topLeft: {
+    marginHorizontal: 25,
+    marginBottom: 5,
+  },
+  bottomLeft: {
+    marginHorizontal: 25,
+  },
+  bottomLeftIn: {
+    flexDirection: "row",
+  },
+  helloUserRight: {
+    width: "30%",
+    // backgroundColor: "green",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  helloUserLeft: {
+    width: "70%",
+    // backgroundColor: 'blue',
+    alignItems: "flex-start",
+    justifyContent: "center",
+  },
+  helloUser: {
+    width: "100%",
+    height: 110,
+    backgroundColor: "#fff",
+    borderBottomRightRadius: 30,
+    borderBottomLeftRadius: 30,
+    flexDirection: "row",
+  },
+  wrapperAllContnet: {
+    flex: 1,
+    backgroundColor: "#cbd5e1",
+  },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
   },
 });
